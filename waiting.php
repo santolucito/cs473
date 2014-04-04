353b038bf400994de47df8f6e4c9a871b6ce89a0
jnom1 <xjunaidx@gmail.com>
2014-04-04 16:33:07 -0400

    Update waiting.php

--- waiting.php
+++ waiting.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "<html><meta http-equiv=\"refresh\" content=\"1;URL='game.php'\"> <br>  Waiting for robot </html>";
+echo "<html><meta http-equiv=\"refresh\" content=\"1;URL='game.php'\"> <br>  Receiving input... processing next turn</html>";
 
 
 
