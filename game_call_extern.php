effa1f820b41d6b75357305a8beb2d1bfeb5fe56
jnom1 <xjunaidx@gmail.com>
2014-04-04 21:07:46 -0400

    Update game_call_extern.php

--- game_call_extern.php
+++ game_call_extern.php
@@ -41,7 +41,7 @@
       //fwrite($client, "\n\n\na\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\na");
      
 	$tosend = "" . chr(0x23) . chr($wait) . chr(109) . chr(109) . chr(0x0) . chr(0x0) . chr(0x23);
-	echo($tosend . "POOOP");
+	echo($tosend);
 	fwrite($client, $tosend);
       //echo stream_get_contents($client);
       fclose($client);
@@ -78,7 +78,7 @@
       //fwrite($client, "\n\n\na\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 
 	$tosend = "" . chr(0x23) . chr($wait) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0);
-	echo($tosend . "POOOP");
+	echo($tosend);
 
 	fwrite($client, $tosend);
 
