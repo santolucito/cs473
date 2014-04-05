0c9282c39996664a46e598b04773755bc02f2915
jnom1 <xjunaidx@gmail.com>
2014-04-04 23:13:53 -0400

    Update game_call_extern.php

--- game_call_extern.php
+++ game_call_extern.php
@@ -77,9 +77,9 @@
 	chr(0) . chr(0) . chr(0) . chr(0)    .   //6
 	chr(0) . chr(0) . chr(0) . chr(23)  ;   //7
 	
-	$data = pack("v", 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF);
+	$data = pack("v", 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF);
 	$strwork = "a\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\na";
- 	socket_write($mysocket, $strwork, 28);
+ 	socket_write($mysocket, $data, 28);
  	socket_close($mysocket);                                                     
  	echo(strlen($data));
  
