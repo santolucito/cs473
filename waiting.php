b122bf2aa2e55b1b176e1f7762e7280c3a145cb4
jnom1 <xjunaidx@gmail.com>
2014-04-13 13:04:34 -0400

    Update waiting.php

--- waiting.php
+++ waiting.php
@@ -1,9 +1,9 @@
 <?php
     session_start();
     $sleeptime = $_SESSION['sleeptime'];
+    $debuginfo = $_SESSION['debug'];
 
-
-echo "<html><meta http-equiv=\"refresh\" content=\"".$sleeptime.";URL='game.php'\"> <br>  Waiting for GLaDOS's input.... </html>";
+echo "<html><meta http-equiv=\"refresh\" content=\"".$sleeptime.";URL='game.php'\"> <br> $debuginfo .  Waiting for GLaDOS's input.... </html>";
 
 
 
