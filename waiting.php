ba787242504825a5820f7740b35a9569ab9baace
jnom1 <xjunaidx@gmail.com>
2014-04-14 04:43:04 -0400

    Update waiting.php

--- waiting.php
+++ waiting.php
@@ -3,12 +3,39 @@
     $sleeptime = $_SESSION['sleeptime'];
     $debuginfo = $_SESSION['debug'];
     
-    
+    function tcp_send($wait){
+$delayfactor = 5;
+
+$mysocket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+socket_connect($mysocket, "caliper.cs.yale.edu", 6667);
+
+$strwork = "\n\n\na\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\na";
+if($wait == 0)
+{
+$strwork = "\n\n\na\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\na";
+}
+else if($wait == (1* $delayfactor))
+{
+$strwork = "\n\n\na\n\n\na\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\na";
+}
+else if($wait == (2* $delayfactor))
+{
+$strwork = "\n\n\na\n\n\na\n\n\na\n\n\n\n\n\n\n\n\n\n\n\n\n\n\na";
+}
+else if ($wait == (3* $delayfactor))
+{
+$strwork = "\n\n\na\n\n\na\n\n\na\n\n\na\n\n\n\n\n\n\n\n\n\n\na";
+}
+socket_write($mysocket, $strwork, 28);
+socket_close($mysocket);
+    }
     
 $sleeptime = 0;
 $debuginfo = "";
 $address = "0";// 'caliper.cs.yale.edu';
 $port = 6667;
+
+tcp_send(5);
 
 $sock = socket_create(AF_INET, SOCK_STREAM, 0); //not sure what the 0 does.
 socket_bind($sock, $address, $port) or die('Could not bind to address');
