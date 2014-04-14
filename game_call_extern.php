5c23223170b6b1a08202fd09e97bb392185eb6b4
jnom1 <xjunaidx@gmail.com>
2014-04-13 21:44:09 -0400

    Update game_call_extern.php

--- game_call_extern.php
+++ game_call_extern.php
@@ -472,7 +472,7 @@ $_SESSION['game_id'] = $current_game_id;
       $state_receive = shell_exec("python gameScript.py ".escapeshellarg($state_send));
       $card_arrays = json_decode($state_receive);
      
-$_SESSION['debug'] = $state_send;
+$_SESSION['debug'] = $state_send . "LASTROUND";
 
     }
     /* $card_arrays = array
