ea17f363f62801d3227184c2344cb6b0bc6632c3
jnom1 <xjunaidx@gmail.com>
2014-04-14 04:48:29 -0400

    Update waiting.php

--- waiting.php
+++ waiting.php
@@ -39,9 +39,9 @@ tcp_send(0);
 /*
 $sock = socket_create(AF_INET, SOCK_STREAM, 0); //not sure what the 0 does.
 socket_bind($sock, $address, $port) or die('Could not bind to address');
-
+*/
 echo "<html><meta http-equiv=\"refresh\" content=\"".$sleeptime.";URL='game.php'\"> <br> $debuginfo .  Waiting for GLaDOS's input.... </html>";
-
+/*
 socket_listen($sock);
 $client = socket_accept($sock);
 
