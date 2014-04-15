1dc6f88ad19a2ab88a4d8bacc2ffb9fa5921dfd7
jnom1 <xjunaidx@gmail.com>
2014-04-14 20:29:51 -0400

    Update waiting.php

--- waiting.php
+++ waiting.php
@@ -136,7 +136,7 @@ socket_bind($sock, $address, $port) or die('Could not bind to address');
       echo "</form>";
 
 
-echo "<html><meta http-equiv=\"refresh\" content=\"".$sleeptime.";URL='game.php'\"> <br> $debuginfo .  Waiting for or processing GLaDOS's input.... </html>";
+echo "<html><meta http-equiv=\"refresh\" content=\"".$sleeptime.";URL='game.php'\"> <br> $debuginfo .  Waiting for GLaDOS's input or processing inputs and preparing next turn.... </html>";
 /*
 socket_listen($sock);
 $client = socket_accept($sock);
