a2c08c3ac921d0fa00557844a6d3f247acb689d0
jnom1 <xjunaidx@gmail.com>
2014-04-15 04:02:48 -0400

    Update waiting.php

--- waiting.php
+++ waiting.php
@@ -154,10 +154,11 @@ echo "<meta http-equiv=\"refresh\" content=\"".$sleeptime.";URL='game.php'\">  <
 
 //if($card_arrays[3][0] == 1)
 //{
-socket_listen($sock);
-$client = socket_accept($sock);
-
-$input = socket_read($client, 1024);
+//socket_listen($sock);
+//$client = socket_accept($sock);
+$torecv;
+$datareceive =  socket_recv($sock , &$torecv , 1024 , 0 );
+//$input = socket_read($client, 1024);
 
 socket_close($client);
 
